59db64ebe1e728aaaece6dc5d51ebfa64797c847
Ravi HPZB <ravikishore@live.com>
2020-11-09 15:25:57 -0600

    Load CSV from GCS to BQ using BigQuery API

---
+++ airflow/dags/sprouts_load_upc.py
@@ -0,0 +1,22 @@
+from google.cloud import bigquery
+
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+# TODO(developer): Set table_id to the ID of the table to create.
+table_id = "shining-landing-763.TEMP_OPS.SPROUTS_UPC_TO_DELETE_UPC_LIST"
+
+job_config = bigquery.LoadJobConfig(
+    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+    source_format=bigquery.SourceFormat.CSV,
+    skip_leading_rows=1,
+    autodetect=True,
+)
+
+uri = "gs://spins-tmp-ext/home/rchatti/sprouts_list_of_upcs.csv"
+load_job = client.load_table_from_uri(uri, table_id, job_config=job_config)  # Make an API request.
+
+load_job.result()  # Waits for the job to complete.
+
+destination_table = client.get_table(table_id)
+print("Loaded {} rows.".format(destination_table.num_rows))
