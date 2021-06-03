68ead30816d7b760558eca68ab55396aa5bb52aa
Ravi HPZB <ravi@RAVIC-ZB.spenceinfo.spins.com>
2021-06-03 18:52:29 -0500

    ParDo One to Many

--- dataflow/pardo_one_to_many.py
+++ dataflow/pardo_one_to_many.py
@@ -1,22 +1,18 @@
 import apache_beam as beam
 
-class BreakIntoWordsDoFn(beam.DoFn):
+class splitWords(beam.DoFn):
     def process(self, element):
-        return [element.split(' ')]
-
+        return element.split()
 
 output = []
-def collect(row):
-    output.append(row)
-    return True
+def collect(word):
+    return output.append(word)
 
 with beam.Pipeline() as P:
-    ## Map Each Input sentence into words tokenized by White Space
-
-    (P
-    | beam.Create(["Hello World,", "This is your Host RaviKishore"])
-    | beam.ParDo(BreakIntoWordsDoFn())
-    | beam.Map(collect)
+    (
+        P   |   beam.Create(['Hello World!', 'This is your host Ravi'])
+            |   beam.ParDo(splitWords())
+            |   beam.Map(collect)
     )
 
 print(output)
