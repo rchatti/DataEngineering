8c657e82bd68d3713418d9235639e903614dca2a
Ravi HPZB <ravi@RAVIC-ZB.spenceinfo.spins.com>
2021-06-04 12:56:42 -0500

    Combine Values (Sum)

--- dataflow/combine.py
+++ dataflow/combine.py
@@ -1,13 +1,35 @@
 import apache_beam as beam
 
-def combinefn(numbers):
-    print(numbers)
-    total = 0
-    for num in numbers:
-        total += num
+"""
+Combine is a Beam transform for combining collections of elements or values in your data. 
 
-    return total
+- MUST provide the function that contains the logic for combining the elements or values. 
+- The Combining function should be commutative and associative, 
+    as the function is not necessarily invoked exactly once on all values with a given key. 
+- the combining function might be called multiple times to perform partial combining on subsets of the value collection.
+
+INPUT: LIST of values
+
+"""
+
+output = []
+def collect(element):
+    yield output.append(element)
+
+def sum(values):
+    # print(type(values))
+
+    op_sum = 0
+    for value in values: 
+        op_sum += value
+    return op_sum
+
 
 with beam.Pipeline() as P:
-    (P | beam.Create([1,2,3,4,5])
-    | beam.CombineGlobally(combinefn))
+    (
+        P   | beam.Create([1,2,3,4,5, 6])
+            | beam.CombineGlobally(sum)
+            | beam.Map(collect)
+    )
+
+print(output)
