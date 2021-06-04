a1192d4c6b48260bc8fc8098d8bf31c98776bac0
Ravi HPZB <ravi@RAVIC-ZB.spenceinfo.spins.com>
2021-06-04 11:02:50 -0500

    Flatmap to Split words in multiple input strings

--- dataflow/flatmap.py
+++ dataflow/flatmap.py
@@ -1,14 +1,22 @@
+
 import apache_beam as beam
+# from hello_world import collect
+
+"""
+FLATMAP: maps an element to multiple elements
+"""
 
 output = []
+
+def collect(word):
+    return output.append(word)
+
 with beam.Pipeline() as P:
     ## Take various strings and split words from all those lists
     (
-        P   
-            | "Create" >> beam.Create(["This is List1", "This is List2"])
-            | 'Split' >> beam.FlatMap(lambda x: x.split())
-            | 'Collect' >> beam.Map(lambda row: output.append(row))
-                        
+        P   | beam.Create(['Apache Beam', 'Google Cloud Dataflow'])
+            | beam.FlatMap(lambda x: x.split())
+            | beam.Map(collect)
     )
 
 print(output)
